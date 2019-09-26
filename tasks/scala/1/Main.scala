acbad97eb29a3ffbb2f2d01ffd2bf5ff929b8f5f
vegtamr <barekov.am.v@gmail.com>
2019-09-26 20:22:49 +0300

    Implemented pascal() function.

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -2,7 +2,7 @@ package recfun
 import common._
 
 object Main {
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
@@ -15,14 +15,18 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if ((c == 0) || (c == r)) {
+      return 1;
+    } else {
+      return pascal(c, r - 1) + pascal(c - 1, r - 1)
+    }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    return false 
   }
 
   /**
@@ -33,6 +37,6 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    return 0
   }
 }

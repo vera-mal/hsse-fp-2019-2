1cefa72aa5ee9de82f3e55cd2fa9046ac1d55b1f
rodchenkov-sn <serod2015@hotmail.com>
2019-10-10 12:35:38 +0300

    Scala #2 fix

--- tasks/scala/2/src/main/scala/funsets/FunSets.scala
+++ tasks/scala/2/src/main/scala/funsets/FunSets.scala
@@ -39,12 +39,12 @@ object FunSets {
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: Set, t: Set): Set = x => s(x) && !t(x)
+  def diff(s: Set, t: Set): Set = x => intersect(s, !t(_))
 
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-  def filter(s: Set, p: Int => Boolean): Set = x => s(x) && p(x)
+  def filter(s: Set, p: Int => Boolean): Set = x => intersect(s, p)
 
 
   /**

f17eeebf8e39022d5b7cc2231d14c820e052f75a
Denis Korolev <denikorol@mail.ru>
2019-10-17 14:55:06 +0300

    Scala 1 Init

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,15 +15,32 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if ((c == 0) || (c == r)) {
+      1
+    } else {
+      pascal(c, r - 1) + pascal(c - 1, r - 1)
+    }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def loop(chars: List[Char], balance: Int): Boolean = {
+      if (chars.isEmpty) {
+        balance == 0
+      } else if (chars.head == '(') {
+        loop(chars.tail, balance + 1)
+      } else if (chars.head == ')') {
+        (balance > 0) && loop(chars.tail, balance - 1)
+      } else {
+        loop(chars.tail, balance)
+      }
+    }
+
+    loop(chars, 0)
   }
+
 
   /**
    * Exercise 3 Counting Change
@@ -32,7 +49,10 @@ object Main {
    * there is 1 way to give change for 5 if you have coins with denomiation
    * 2 and 3: 2+3.
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
-
+  def countChange(money: Int, coins: List[Int]): Int = (money, coins) match {
+    case (_, Nil)             => 0
+    case (tmp, _) if tmp < 0  => 0
+    case (0, _)               => 1
+    case (tmp, _)             => countChange(tmp, coins.tail) + countChange(tmp - coins.head, coins)
   }
 }

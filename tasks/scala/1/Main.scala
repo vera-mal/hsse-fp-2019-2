792be725657f3fa2dad7655bbeb56c95f675b0e0
IvlevaAnna <annaiva19@mail.ru>
2019-10-20 23:39:41 +0300

    Add lab 1 scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,14 +15,31 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+	if (c == 0 || c == r)
+      1
+    else
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+	def balanceIter(position: Int, openedBraces: Int): Boolean = {
+      if (position == chars.length)
+        openedBraces == 0
+      else if (chars(position) == '(')
+        balanceIter(position + 1, openedBraces + 1)
+      else if (chars(position) == ')')
+        if (openedBraces == 0)
+          false
+        else
+          balanceIter(position + 1, openedBraces - 1)
+      else
+        balanceIter(position + 1, openedBraces)
+    }
+
+    balanceIter(0, 0)
   }
 
   /**
@@ -33,6 +50,11 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+	if (money == 0)
+      1
+    else if (money < 0 || coins.isEmpty)
+      0
+    else
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
 }

f87905d4ce8e116304b4ae3634dcd1a5110beb58
aleksey52 <afanasiev2.aa@edu.spbstu.ru>
2019-10-20 19:57:59 +0300

    Scala_lab_1: first commit

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -9,20 +9,40 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    println("Parentheses Balancing")
+    val inputChars: List[Char] = scala.io.StdIn.readLine().toList
+    println(balance(inputChars))
+    println()
+
+    println("Counting Change")
+    println("Enter money")
+    val money: Int = scala.io.StdIn.readInt()
+    println("Enter coins")
+    val coins: List[Int] = scala.io.StdIn.readLine().filter(_!='\n').split(' ').map(_.toInt).toList
+    println(countChange(money, coins))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if ((c == 0) || (r == 0) || (c == r)) { 1 }
+    else { pascal(c - 1, r - 1) + pascal(c, r - 1) }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    var countOpenParenthesis: Int = 0
+    var countCloseParenthesis: Int = 0
+    for (i <- 0 until chars.length) {
+      if (chars(i) == '(') { countOpenParenthesis += 1 }
+      if (chars(i) == ')') { countCloseParenthesis += 1 }
+    }
+    if (countOpenParenthesis == countCloseParenthesis) { true }
+    else { false }
   }
 
   /**
@@ -33,6 +53,8 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if ((coins.isEmpty) || (money < 0)) { 0 }
+    else if (money == 0) { 1 }
+    else { countChange(money, coins.tail) + countChange(money - coins.head, coins) }
   }
 }

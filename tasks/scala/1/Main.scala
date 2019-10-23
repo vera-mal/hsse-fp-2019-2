831b80e436c30348a0155c41a2422ad3da36a7a0
Vyacheslav Seleznev <vya4eslav.seleznev@gmail.com>
2019-10-23 21:59:28 +0300

    Lab 1 scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,12 +1,17 @@
 package recfun
-import common._
 
-object Main {
-  def main(args: Array[String]) {
+object Main
+{
+  def main(args: Array[String])
+  {
     println("Pascal's Triangle")
-    for (row <- 0 to 10) {
+    for (row <- 0 to 10)
+    {
       for (col <- 0 to row)
+      {
         print(pascal(col, row) + " ")
+      }
+
       println()
     }
   }
@@ -14,15 +19,49 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = {
+  def pascal(c: Int, r: Int): Int =
+  {
+    if ((c < 0) || (r < 0))
+    {
+      throw new IllegalArgumentException("Incorrect arguments.")
+    }
 
+    if ((c == 0) || (c == r))
+    {
+      1
+    }
+    else
+    {
+      pascal(c, r - 1) + pascal(c - 1, r - 1)
+    }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
-  def balance(chars: List[Char]): Boolean = {
-   
+  def balance(chars: List[Char]): Boolean =
+  {
+    def checkBalance(chars: List[Char], count: Int): Boolean =
+    {
+      if (chars.isEmpty)
+      {
+        count == 0
+      }
+      else if (chars.head == '(')
+      {
+        checkBalance(chars.tail, count + 1)
+      }
+      else if (chars.head == ')')
+      {
+        (count > 0) && checkBalance(chars.tail, count - 1)
+      }
+      else
+      {
+        checkBalance(chars.tail, count)
+      }
+    }
+
+    checkBalance(chars, 0)
   }
 
   /**
@@ -32,7 +71,19 @@ object Main {
    * there is 1 way to give change for 5 if you have coins with denomiation
    * 2 and 3: 2+3.
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
-
+  def countChange(money: Int, coins: List[Int]): Int =
+  {
+    if (money == 0)
+    {
+      1;
+    }
+    else if ((money < 0) || coins.isEmpty)
+    {
+      0;
+    }
+    else
+    {
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    }
   }
 }

5d8680e5c5b7b67dabccbeaf2138dfacf7404ba7
dimka_9910 <dimka99-10@mail.ru>
2019-10-29 00:53:42 +0300

    1st task

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,29 +1,58 @@
 package recfun
-import common._
 
 object Main {
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
     println("Pascal's Triangle")
-    for (row <- 0 to 10) {
-      for (col <- 0 to row)
-        print(pascal(col, row) + " ")
+    for (y <- 0 to 10) {
+      for (x <- 0 to y)
+        print(pascal(x, y) + " ")
       println()
     }
+    
+    println("\nParentheses Balancing ")
+    println(balance("()(())()".toCharArray.toList))
+    println(balance("()(()()".toCharArray.toList))
+    
+    println("\nCoins change problem")
+    println(countChange(5, List(1, 2, 3)))
+    
   }
-
 
   /**
    * Exercise 1
-   */
-  def pascal(c: Int, r: Int): Int = {
-
+   */ 
+  def pascal(x: Int, y: Int): Int = {
+    if ((x == 0) || (x == y))  1; 
+    else 
+    {
+      pascal(x, y - 1) + pascal(x - 1, y - 1);
+    }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def check(chars: List[Char], count: Int): Boolean = {
+      if (chars.isEmpty) 
+      {
+        count == 0
+      } 
+      else if (chars.head == '(') 
+      {
+        check(chars.tail, count + 1)
+      } 
+      else if (chars.head == ')') 
+      {
+        (count > 0) && check(chars.tail, count - 1)
+      } 
+      else 
+      {
+        check(chars.tail, count)
+      }
+    }
+
+    check(chars, 0)
   }
 
   /**
@@ -34,6 +63,12 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0)  1;
+    else 
+    if ((money < 0) || coins.isEmpty) 0;
+    else 
+    {
+      countChange(money - coins.head, coins) + countChange(money, coins.tail);
+    }
   }
 }

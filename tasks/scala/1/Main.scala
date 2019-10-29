69e3b082442681bfd6bf62dc9a77aabbe73cfd90
BesedinDaniil <d.besedin.0@ya.ru>
2019-10-29 03:51:38 +0300

    Init first task

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,4 +1,5 @@
 package recfun
+
 import common._
 
 object Main {
@@ -12,27 +13,50 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
+    * Exercise 1
+    */
+
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r)
+      1
+    else
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
-   * Exercise 2 Parentheses Balancing
-   */
+    * Exercise 2 Parentheses Balancing
+    */
+
   def balance(chars: List[Char]): Boolean = {
-   
+    def check(chars: List[Char], count: Int): Boolean = {
+      if (chars.isEmpty) {
+        count == 0
+      } else if (chars.head == '(') {
+        check(chars.tail, count + 1)
+      } else if (chars.head == ')') {
+        (count > 0) && check(chars.tail, count - 1)
+      } else {
+        check(chars.tail, count)
+      }
+    }
+
+    check(chars, 0)
   }
 
   /**
-   * Exercise 3 Counting Change
-   * Write a recursive function that counts how many different ways you can make
-   * change for an amount, given a list of coin denominations. For example,
-   * there is 1 way to give change for 5 if you have coins with denomiation
-   * 2 and 3: 2+3.
-   */
-  def countChange(money: Int, coins: List[Int]): Int = {
+    * Exercise 3 Counting Change
+    * Write a recursive function that counts how many different ways you can make
+    * change for an amount, given a list of coin denominations. For example,
+    * there is 1 way to give change for 5 if you have coins with denomiation
+    * 2 and 3: 2+3.
+    */
 
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0)
+      1
+    else if (money < 0 || coins.isEmpty)
+      0
+    else
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
 }

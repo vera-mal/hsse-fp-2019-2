33baa4fcc7b53d38ff790df9205bddf983f8e3eb
Albert Leinsoo <albertleinsoo@mail.ru>
2019-10-29 22:57:24 +0300

    Add Lab 1

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,7 +1,5 @@
-package recfun
-import common._
-
 object Main {
+
   def main(args: Array[String]) {
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
@@ -11,28 +9,65 @@ object Main {
     }
   }
 
-  /**
-   * Exercise 1
-   */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r)
+      1
+    else
+      (pascal(c - 1, r - 1) + pascal(c, r - 1))
   }
 
-  /**
-   * Exercise 2 Parentheses Balancing
-   */
   def balance(chars: List[Char]): Boolean = {
-   
+
+    def subFunction(chars: List[Char], numOpens: Int): Boolean = {
+
+      if (chars.isEmpty) {
+        numOpens == 0
+      }
+      else {
+
+        val head = chars.head
+        val number = 0
+
+        if (head == '(') numOpens + 1
+        else if (head == '[') numOpens + 1
+        else if (head == '{') numOpens + 1
+        else if (head == ')') numOpens - 1
+        else if (head == ']') numOpens - 1
+        else if (head == '}') numOpens - 1
+        else numOpens
+
+        if (number >= 0) subFunction(chars.tail, number)
+        else false
+
+      }
+
+    }
+
+    subFunction(chars, 0)
+
   }
 
-  /**
-   * Exercise 3 Counting Change
-   * Write a recursive function that counts how many different ways you can make
-   * change for an amount, given a list of coin denominations. For example,
-   * there is 1 way to give change for 5 if you have coins with denomiation
-   * 2 and 3: 2+3.
-   */
+
+
   def countChange(money: Int, coins: List[Int]): Int = {
 
+    var amount = 0;
+
+    def subFunction(money: Int, coins: List[Int]): Any = {
+      if (!coins.isEmpty) {
+        if (money > coins.head) {
+          subFunction(money - coins.head, coins)
+          subFunction(money, coins.tail)
+        }
+        else if (money < coins.head) {
+          subFunction(money, coins.tail)
+        }
+        else if (money-coins.head == 0) {
+          amount += 1
+        }
+      }
+    }
+    subFunction(money, coins.sorted)
+    amount
   }
 }

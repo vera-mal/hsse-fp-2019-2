187905986c6775e4607f9000a63128bb59207a8e
Alexandra <adlovlac@gmail.com>
2019-10-31 22:44:07 +0300

    Lab1, initial commit

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,28 +1,52 @@
-package recfun
-import common._
+//package recfun
+
 
 object Main {
   def main(args: Array[String]) {
+
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
     }
+    println("Check for balance")
+    var listWrong1 = List('(', ')', '(')
+    var listWrong2 = List(')', '(')
+    var listCorrect = List('(', '(', ')', '(', ')', ')')
+    println(balance(listWrong1))
+    println(balance(listWrong2))
+    println(balance(listCorrect))
+
+    println("Count change")
+    var coins = List(2, 3)
+    println(countChange(5, coins))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if ((c < 0) || (c > r)) {0}
+    else if (c == 0)  {1}
+    else  {pascal(c - 1, r - 1) + pascal(c, r - 1)}
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    var counter = 0
+    def proccessChar(element: Char) = {
+      if (element == '(') counter = counter + 1
+      else if (element == ')') counter = counter - 1
+    }
+    for (char <- chars) {
+      proccessChar(char)
+      if (counter < 0) return false
+    }
+    if (counter == 0) return true
+    else return false
   }
 
   /**
@@ -33,6 +57,15 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0)
+    {
+      return 1
+    }
+    if (money < 0 || coins.isEmpty)
+    {
+      return 0
+    }
+    return countChange(money, coins.tail) + countChange(money - coins.head, coins)
   }
 }
+

639eb31802a230de6890a7a7ff363926275e2bd6
ZakharovaAlyona <alenazakharovaspb@yandex.ru>
2019-11-05 15:50:30 +0300

    lab1 initial commit

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,5 @@
 package recfun
-import common._
+//import common._
 
 object Main {
   def main(args: Array[String]) {
@@ -15,14 +15,26 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r)
+      1
+    else {
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def f(chars: List[Char], open: Int): Boolean = {
+      if (chars.isEmpty) open == 0
+      else if (chars.head == '(') f(chars.tail, open + 1)
+      else if (chars.head == ')')
+        if (open > 0) f(chars.tail, open - 1)
+        else false
+      else f(chars.tail, open)
+    }
+    f(chars, 0)
   }
 
   /**
@@ -33,6 +45,19 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0)
+      1
+    else if (money < 0 || coins.isEmpty)
+      0
+    else
+      countChange(money, coins.tail) + countChange(money - coins.head, coins)
   }
+
+  println("Parentheses balancing")
+  println(balance("((asdasd)))".toList))
+  println(balance("(10-5)*(a-b)".toList))
+
+  val listik = List(1, 2)
+  println("Counting change")
+  println(countChange(4, listik))
 }

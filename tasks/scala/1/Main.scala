f05e3ef87edb8a0f1d8c2c102243af55b285c8e0
chugaynov <chugaynov.v300100@mail.ru>
2019-11-06 22:56:49 +0300

    init commit of scala.first

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,4 @@
 package recfun
-import common._
 
 object Main {
   def main(args: Array[String]) {
@@ -9,20 +8,45 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+
+    println("Parentheses Balancing")
+    val chars1 = List('(', '(', ')', ')', '(')
+    val chars2 = List('(', '(', ')', ')')
+    println(balance(chars1))
+    println(balance(chars2))
+
+
+    println("Counting Change")
+    val coins1 = List(2, 3, 4, 5)
+    val coins2 = List(2, 3)
+    val money = 10
+    println(countChange(money, coins1))
+    println(countChange(money, coins2))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c != 0 && c != r) pascal(c - 1, r - 1) + pascal(c, r - 1) else 1
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
+
   def balance(chars: List[Char]): Boolean = {
-   
+    def checkBalance(chars: List[Char], bracketsCounter: Int = 0): Boolean = {
+      if (chars.isEmpty) bracketsCounter == 0
+      else chars.head match {
+        case '(' => checkBalance(chars.tail, bracketsCounter + 1)
+        case ')' => bracketsCounter > 0 && checkBalance(chars.tail, bracketsCounter - 1)
+        case _ => checkBalance(chars.tail, bracketsCounter)
+      }
+    }
+
+    checkBalance(chars)
   }
 
   /**
@@ -33,6 +57,8 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if ((money < 0) || coins.isEmpty) 0
+    else if (money == 0) 1
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
 }
